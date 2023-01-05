fbd74b3da508d894f598b114135f9339f33d835e
Purp_Killah <46106827+PurpleBoychik@users.noreply.github.com>
2023-01-05 21:29:36 +0300

    adaptive all sections

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#navBtn');
+const navBtnImg = document.querySelector('#navBtnImg');
+
+navBtn.addEventListener('click', openMenu = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = ('img/icons/navClose.svg');
+    } else {
+        navBtnImg.src = ('img/icons/navOpen.svg');
+    }
+});
